6eddfe3d63a8ef0b6a694bed7d1c34aa2f320e2c
giumas <giumas@yahoo.it>
2019-01-29 21:30:46 -0500

    added Report class

--- examples/app/ex_report.py
+++ examples/app/ex_report.py
@@ -3,15 +3,15 @@ import logging
 
 from PySide2 import QtWidgets
 
+from hyo2.abc.app.report import Report
+from hyo2.abc.lib.lib_info import LibInfo
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-from hyo2.abc.app.report import Report
-from hyo2.abc.lib.lib_info import LibInfo
-from hyo2.abc.app.app_info import AppInfo
-
 app = QtWidgets.QApplication([])
 
-r = Report()
+li = LibInfo()
+r = Report(lib_name=li.lib_name, lib_version=li.lib_version)
 
-sys.exit(app.exec_())
+# sys.exit(app.exec_())
