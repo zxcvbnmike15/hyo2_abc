388bb1c9a74a0b0874151096c01e6b6c99ac8045
giumas <giumas@yahoo.it>
2019-06-03 21:23:46 -0400

    improved coverage for Report test

--- tests/app/test_report.py
+++ tests/app/test_report.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import platform
 import unittest
 
@@ -5,9 +6,14 @@ from PySide2 import QtWidgets
 
 from hyo2.abc.app.report import Report
 from hyo2.abc.lib.lib_info import LibInfo
+from hyo2.abc.lib.testing_paths import TestingPaths
 
 
 class TestABCLibReport(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.testing = TestingPaths(root_folder=Path(__file__).parent.parent.parent.resolve())
 
     @unittest.skipIf(platform.system() in ['Linux', ], "It crashes on Linux")
     def test_init(self):
@@ -16,7 +22,28 @@ class TestABCLibReport(unittest.TestCase):
             QtWidgets.QApplication([])
 
         li = LibInfo()
-        _ = Report(lib_name=li.lib_name, lib_version=li.lib_version)
+        rep = Report(lib_name=li.lib_name, lib_version=li.lib_version)
+
+        rep += "Test [CHECK]"
+        rep += "OK"
+
+        rep += "Test [CHECK]"
+        rep += "test"
+        rep += "test"
+        rep += "test"
+
+        rep += "test [SKIP_CHK]"
+
+        rep += "skip [SKIP_REP]"
+
+        rep += "End [TOTAL]"
+        rep += "Check 1 - Test"
+        rep += "Check 2 - Test"
+
+        rep.display()
+
+        rep.generate_pdf(path=str(self.testing.output_data_folder().joinpath('test.pdf')),
+                         title="Test Document", use_colors=True, small=True)
 
 
 def suite():
