830c017fdfd64acae810344cf2fb947e250eadb1
giumas <giumas@yahoo.it>
2020-08-03 19:25:52 +0200

    added CARIS Support Files 2020.v3

--- hyo2/abc/__init__.py
+++ hyo2/abc/__init__.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 name = "ABC"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "gmasetti@ccom.unh.edu"
 __license__ = "LGPLv3 license"
 __copyright__ = "Copyright 2020 University of New Hampshire, Center for Coastal and Ocean Mapping"
