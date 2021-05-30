ccebb3b9ac1508d4ac4d71c432530b90677678b0
Giuseppe Masetti <gimas@gst.dk>
2021-05-30 13:32:35 +0200

    removed warnings for the WebRenderer

--- hyo2/abc/app/web_renderer.py
+++ hyo2/abc/app/web_renderer.py
@@ -2,17 +2,33 @@ import logging
 
 from PySide2.QtCore import QEventLoop, QTimer, QUrl
 from PySide2.QtWidgets import QApplication
-from PySide2.QtWebEngineWidgets import QWebEngineView
+from PySide2.QtWebEngineWidgets import QWebEnginePage, QWebEngineView, QWebEngineSettings
 
 logger = logging.getLogger(__name__)
+
+
+class WebEnginePage(QWebEnginePage):
+
+    def javaScriptConsoleMessage(self, level, message, line_number, source_id):
+        if ("slideshare" not in source_id) and ("hydroffice.org" not in source_id):
+            logger.debug("QWebEngine: %s[#%d] -> %s" % (source_id, line_number, message))
 
 
 class WebRenderer(QWebEngineView):
 
     def __init__(self, make_app: bool = False):
         if make_app:
+            # os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--enable-logging --log-level=3"
             self._app = QApplication([])
         QWebEngineView.__init__(self)
+
+        self.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
+        self.settings().setAttribute(QWebEngineSettings.JavascriptCanOpenWindows, True)
+        self.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessRemoteUrls, True)
+        self.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessFileUrls, True)
+
+        self.page = WebEnginePage(self)
+        self.setPage(self.page)
 
     def open(self, url: str, timeout: int = 10):
         """Wait for download to complete and return result"""
