a590c3dd376df3fdd05dd49c8ffc966a0fa3185e
giumas <giuseppemasetti@gmail.com>
2022-02-08 19:20:56 +0100

    updates for NOAA Caris support files v.2022.2

--- hyo2/abc/__init__.py
+++ hyo2/abc/__init__.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 name = "ABC"
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 __author__ = "gmasetti@ccom.unh.edu"
 __license__ = "LGPLv3 license"
-__copyright__ = "Copyright 2021 University of New Hampshire, Center for Coastal and Ocean Mapping"
+__copyright__ = "Copyright 2022 University of New Hampshire, Center for Coastal and Ocean Mapping"
